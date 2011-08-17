0dbb571b5b6c053dd7bad8bccb888b730e7e0971
croesch <christianroesch@gmx.net>
2011-08-17 20:26:22 +0200

    Implemented additional methods for Argument, to make them more handy

--- src/main/java/com/github/croesch/Argument.java
+++ src/main/java/com/github/croesch/Argument.java
@@ -1,5 +1,8 @@
 package com.github.croesch;
 
+import java.util.EnumMap;
+import java.util.Map;
+
 /**
  * Enumeration of all possible command line arguments for the debugger.
  * 
@@ -12,10 +15,16 @@ enum Argument {
   HELP,
 
   /** argument to view the version of the debugger */
-  VERSION;
+  VERSION,
+
+  /** argument to define the debug level of the program */
+  DEBUG_LEVEL (1);
 
   /** the different ways this argument can be called */
   private final String[] args = new String[2];
+
+  /** the number of parameters that argument requires */
+  private final int numOfParams;
 
   /**
    * Constructs a new argument with its name as long argument and its first letter as short argument. For example
@@ -25,13 +34,35 @@ enum Argument {
    * <li><code>-a</code></li>
    * </ul>
    * <br>
-   * <b>Note:</b> A <code>_</code> in the name will be translated to a <code>-</code>.
+   * <b>Note:</b> A <code>_</code> in the name will be translated to a <code>-</code>.<br>
+   * This argument will require no parameters.
    * 
    * @since Date: Aug 13, 2011
+   * @see #Argument(int)
    */
   private Argument() {
+    this(0);
+  }
+
+  /**
+   * Constructs a new argument with its name as long argument and its first letter as short argument. For example
+   * ARGUMENT will result in:
+   * <ul>
+   * <li><code>--argument</code></li>
+   * <li><code>-a</code></li>
+   * </ul>
+   * <br>
+   * <b>Note:</b> A <code>_</code> in the name will be translated to a <code>-</code>. <br>
+   * This argument will require the passed number of parameters.
+   * 
+   * @param nop the number of parameters this argument requires
+   * @since Date: Aug 17, 2011
+   * @see #Argument()
+   */
+  private Argument(final int nop) {
     this.args[0] = "--" + this.name().toLowerCase().replaceAll("_", "-");
     this.args[1] = "-" + this.name().substring(0, 1).toLowerCase();
+    this.numOfParams = nop;
   }
 
   /**
@@ -44,6 +75,16 @@ enum Argument {
    */
   private boolean matches(final String m) {
     return m != null && (m.equals(args[0]) || m.equals(args[1]));
+  }
+
+  /**
+   * Returns the number of parameters this argument requires.
+   * 
+   * @since Date: Aug 17, 2011
+   * @return the number of parameters for that argument.
+   */
+  private int getNumberOfParameters() {
+    return this.numOfParams;
   }
 
   /**
@@ -62,5 +103,35 @@ enum Argument {
       }
     }
     return null;
+  }
+
+  /**
+   * Converts a given array of {@link String}s into a {@link Map} that contains an entry for each valid {@link Argument}
+   * and the possible parameters belonging to it.
+   * 
+   * @since Date: Aug 17, 2011
+   * @param args the array of {@link String}s
+   * @return the {@link Map} that contains pairs of {@link Argument}s and arrays of strings that contain all parameters
+   *         for that argument.
+   */
+  static Map<Argument, String[]> createArgumentList(final String[] args) {
+    final Map<Argument, String[]> map = new EnumMap<Argument, String[]>(Argument.class);
+    if (args != null) {
+      for (int i = 0; i < args.length; ++i) {
+        if (args[i] != null) {
+          final Argument arg = of(args[i]);
+          if (arg != null) {
+            final String[] params = new String[arg.getNumberOfParameters()];
+            if (i + arg.getNumberOfParameters() < args.length) {
+              for (int j = 0; j < arg.getNumberOfParameters(); ++j) {
+                params[j] = args[j + i + 1];
+              }
+              map.put(arg, params);
+            }
+          }
+        }
+      }
+    }
+    return map;
   }
 }
--- src/test/java/com/github/croesch/ArgumentTest.java
+++ src/test/java/com/github/croesch/ArgumentTest.java
@@ -16,17 +16,136 @@ public class ArgumentTest {
    * Test method for {@link Argument#of(String)}.
    */
   @Test
-  public final void testOf() {
+  public final void testOf_DifferentValues() {
     assertThat(Argument.of("--help")).isSameAs(Argument.HELP);
     assertThat(Argument.of("-h")).isSameAs(Argument.HELP);
 
     assertThat(Argument.of("--version")).isSameAs(Argument.VERSION);
     assertThat(Argument.of("-v")).isSameAs(Argument.VERSION);
 
+    assertThat(Argument.of("--debug-level")).isSameAs(Argument.DEBUG_LEVEL);
+    assertThat(Argument.of("-d")).isSameAs(Argument.DEBUG_LEVEL);
+  }
+
+  /**
+   * Test method for {@link Argument#of(String)}.
+   */
+  @Test
+  public final void testOf_Unkown() {
     assertThat(Argument.of(null)).isNull();
     assertThat(Argument.of("")).isNull();
     assertThat(Argument.of(" ")).isNull();
     assertThat(Argument.of("HELP")).isNull();
     assertThat(Argument.of("-help")).isNull();
+    assertThat(Argument.of("-debug-level")).isNull();
+    assertThat(Argument.of("--debug_level")).isNull();
+  }
+
+  /**
+   * Test method for {@link Argument#createArgumentList(String[])}.
+   */
+  @Test
+  public final void testCreateArgumentList_NullDeliversEmptyMap() {
+    assertThat(Argument.createArgumentList(null)).isEmpty();
+  }
+
+  /**
+   * Test method for {@link Argument#createArgumentList(String[])}.
+   */
+  @Test
+  public final void testCreateArgumentList_EmptyArrayDeliversEmptyMap() {
+    assertThat(Argument.createArgumentList(new String[] {})).isEmpty();
+  }
+
+  /**
+   * Test method for {@link Argument#createArgumentList(String[])}.
+   */
+  @Test
+  public final void testCreateArgumentList_NullArgumentProducesNoEntry() {
+    assertThat(Argument.createArgumentList(new String[] { null })).isEmpty();
+  }
+
+  /**
+   * Test method for {@link Argument#createArgumentList(String[])}.
+   */
+  @Test
+  public final void testCreateArgumentList_UnknownArgumentProducesNoEntry() {
+    assertThat(Argument.createArgumentList(new String[] { "" })).isEmpty();
+    assertThat(Argument.createArgumentList(new String[] { " " })).isEmpty();
+    assertThat(Argument.createArgumentList(new String[] { "-help" })).isEmpty();
+  }
+
+  /**
+   * Test method for {@link Argument#createArgumentList(String[])}.
+   */
+  @Test
+  public final void testCreateArgumentList_HelpInArray() {
+    String[] args = new String[] { "-h" };
+
+    assertThat(Argument.createArgumentList(args).keySet()).containsOnly(Argument.HELP);
+    assertThat(Argument.createArgumentList(args).get(Argument.HELP)).isEmpty();
+
+    args = new String[] { "--help" };
+    assertThat(Argument.createArgumentList(args).keySet()).containsOnly(Argument.HELP);
+    assertThat(Argument.createArgumentList(args).get(Argument.HELP)).isEmpty();
+  }
+
+  /**
+   * Test method for {@link Argument#createArgumentList(String[])}.
+   */
+  @Test
+  public final void testCreateArgumentList_VersionInArray() {
+    String[] args = new String[] { "-v" };
+
+    assertThat(Argument.createArgumentList(args).keySet()).containsOnly(Argument.VERSION);
+    assertThat(Argument.createArgumentList(args).get(Argument.VERSION)).isEmpty();
+
+    args = new String[] { "--version" };
+    assertThat(Argument.createArgumentList(args).keySet()).containsOnly(Argument.VERSION);
+    assertThat(Argument.createArgumentList(args).get(Argument.VERSION)).isEmpty();
+  }
+
+  /**
+   * Test method for {@link Argument#createArgumentList(String[])}.
+   */
+  @Test
+  public final void testCreateArgumentList_DebugLevelInArray() {
+    String[] args = new String[] { "-d" };
+
+    assertThat(Argument.createArgumentList(args).keySet()).isEmpty();
+
+    args = new String[] { "-d", "2" };
+    assertThat(Argument.createArgumentList(args).keySet()).containsOnly(Argument.DEBUG_LEVEL);
+    assertThat(Argument.createArgumentList(args).get(Argument.DEBUG_LEVEL)).containsOnly("2");
+
+    args = new String[] { "--debug-level", "2" };
+    assertThat(Argument.createArgumentList(args).keySet()).containsOnly(Argument.DEBUG_LEVEL);
+    assertThat(Argument.createArgumentList(args).get(Argument.DEBUG_LEVEL)).containsOnly("2");
+  }
+
+  /**
+   * Test method for {@link Argument#createArgumentList(String[])}.
+   */
+  @Test
+  public final void testCreateArgumentList() {
+    final String[] args = new String[] { "-h", "-v", null, "--help", "--xxno-argument", "null" };
+
+    assertThat(Argument.createArgumentList(args)).hasSize(2);
+    assertThat(Argument.createArgumentList(args).keySet()).containsOnly(Argument.VERSION, Argument.HELP);
+    assertThat(Argument.createArgumentList(args).get(Argument.VERSION)).isEmpty();
+    assertThat(Argument.createArgumentList(args).get(Argument.HELP)).isEmpty();
+  }
+
+  /**
+   * Test method for {@link Argument#createArgumentList(String[])}.
+   */
+  @Test
+  public final void testCreateArgumentList2() {
+    final String[] args = new String[] { "-h", "-v", null, "--xxno-argument", "null" };
+
+    assertThat(Argument.createArgumentList(args)).hasSize(2);
+    assertThat(Argument.createArgumentList(args).keySet()).containsOnly(Argument.VERSION, Argument.HELP);
+    assertThat(Argument.createArgumentList(args).get(Argument.HELP)).isEmpty();
+    assertThat(Argument.createArgumentList(args).get(Argument.VERSION)).isEmpty();
   }
 }
