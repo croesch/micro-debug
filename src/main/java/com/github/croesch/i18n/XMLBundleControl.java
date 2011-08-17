d3185ef848305ae845a21dd1f4d8712f1d1604c7
croesch <christianroesch@gmx.net>
2011-08-17 23:49:50 +0200

    moved XMLResourceBundle in a seperate file

--- src/main/java/com/github/croesch/i18n/XMLBundleControl.java
+++ src/main/java/com/github/croesch/i18n/XMLBundleControl.java
@@ -6,11 +6,8 @@ import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Enumeration;
 import java.util.List;
 import java.util.Locale;
-import java.util.Properties;
 import java.util.ResourceBundle;
 import java.util.ResourceBundle.Control;
 
@@ -71,42 +68,5 @@ final class XMLBundleControl extends Control {
       }
     }
     return bundle;
-  }
-}
-
-/**
- * Resource bundle from javadoc-example of {@link Control} for XML-based bundles.
- * 
- * @author croesch
- * @since Date: Aug 17, 2011
- */
-final class XMLResourceBundle extends ResourceBundle {
-
-  /** the properties that contain the content of the xml file */
-  private final Properties props;
-
-  /**
-   * Constructs the resource bundle from javadoc-example of {@link Control} for XML-based bundles.
-   * 
-   * @since Date: Aug 17, 2011
-   * @param stream the xml-stream to read the data from
-   * @throws IOException in case of IO problem, or if stream is <code>null</code>
-   */
-  XMLResourceBundle(final InputStream stream) throws IOException {
-    if (stream == null) {
-      throw new IOException();
-    }
-    this.props = new Properties();
-    this.props.loadFromXML(stream);
-  }
-
-  @Override
-  protected Object handleGetObject(final String key) {
-    return this.props.getProperty(key);
-  }
-
-  @Override
-  public Enumeration<String> getKeys() {
-    return Collections.enumeration(this.props.stringPropertyNames());
   }
 }
--- src/test/java/com/github/croesch/i18n/XMLBundleControlTest.java
+++ src/test/java/com/github/croesch/i18n/XMLBundleControlTest.java
@@ -3,10 +3,7 @@ package com.github.croesch.i18n;
 import static org.fest.assertions.Assertions.assertThat;
 
 import java.io.IOException;
-import java.util.Collections;
 import java.util.Locale;
-import java.util.MissingResourceException;
-import java.util.ResourceBundle;
 
 import org.junit.Test;
 
@@ -120,62 +117,5 @@ public class XMLBundleControlTest {
     final XMLBundleControl control = new XMLBundleControl();
     assertThat(control.newBundle("baseName", Locale.getDefault(), "xml", ClassLoader.getSystemClassLoader(), true))
       .isNull();
-  }
-
-  /**
-   * Test method for {@link XMLResourceBundle}.
-   */
-  @Test(expected = MissingResourceException.class)
-  public void testGetString_MRE1() {
-    final ResourceBundle bundle = ResourceBundle.getBundle("resourcebundle-test/unknown", new XMLBundleControl());
-    bundle.getString("key");
-  }
-
-  /**
-   * Test method for {@link XMLResourceBundle}.
-   */
-  @Test(expected = MissingResourceException.class)
-  public void testGetString_MRE2() {
-    final ResourceBundle bundle = ResourceBundle.getBundle("resourcebundle-test/file", new XMLBundleControl());
-    bundle.getString("key1");
-  }
-
-  /**
-   * Test method for {@link XMLResourceBundle}.
-   */
-  @Test
-  public void testGetString_English() {
-    Locale.setDefault(Locale.ENGLISH);
-    final ResourceBundle bundle = ResourceBundle.getBundle("resourcebundle-test/file", new XMLBundleControl());
-    assertThat(bundle.getString("key")).isEqualTo("content");
-  }
-
-  /**
-   * Test method for {@link XMLResourceBundle}.
-   */
-  @Test
-  public void testGetString_German() {
-    Locale.setDefault(Locale.GERMAN);
-    final ResourceBundle bundle = ResourceBundle.getBundle("resourcebundle-test/file", new XMLBundleControl());
-    assertThat(bundle.getString("key")).isEqualTo("Inhalt");
-    assertThat(bundle.getString("key")).isEqualTo("Inhalt");
-  }
-
-  /**
-   * Test method for {@link XMLResourceBundle#XMLResourceBundle(java.io.InputStream)}.
-   */
-  @Test(expected = IOException.class)
-  public void testXMLResourceBundle() throws IOException {
-    new XMLResourceBundle(null);
-  }
-
-  /**
-   * Test method for {@link XMLResourceBundle#getKeys()}.
-   */
-  @Test
-  public void testGetKeys() throws IOException {
-    Locale.setDefault(Locale.ENGLISH);
-    final ResourceBundle bundle = ResourceBundle.getBundle("resourcebundle-test/file", new XMLBundleControl());
-    assertThat(Collections.list(bundle.getKeys())).containsOnly("key", "yet another key", "other key");
   }
 }
