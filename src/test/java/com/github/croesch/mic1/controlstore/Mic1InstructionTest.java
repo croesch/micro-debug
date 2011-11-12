29619c0cbf3b882e2b2e78548d5e87fa1cc55bf3
croesch <christianroesch@gmx.net>
2011-11-12 19:11:09 +0100

    implemented some tests

--- src/test/java/com/github/croesch/mic1/controlstore/Mic1InstructionTest.java
+++ src/test/java/com/github/croesch/mic1/controlstore/Mic1InstructionTest.java
@@ -28,27 +28,145 @@ public class Mic1InstructionTest {
   }
 
   @Test
-  public void testHashCode() {
-    System.err.println("not yet implemented");
-  }
-
-  @Test
   public void testToString() {
-    System.err.println("not yet implemented");
+    printMethodName();
+
+    final BitSet bits = new BitSet();
+
+    this.instruction = new Mic1Instruction(0, bits, 0);
+    assertThat(this.instruction.toString()).isEqualTo("goto 0x0");
+
+    bits.set(2);
+    bits.set(4);
+    bits.set(6);
+    bits.set(8);
+    bits.set(10);
+    bits.set(12);
+    bits.set(14);
+    bits.set(16);
+    this.instruction = new Mic1Instruction(47, bits, 5);
+    assertThat(this.instruction.toString()).isEqualTo("Z=OPC=CPP=SP=LV>>1;if (Z) goto 0x12F; else goto 0x2F");
+
+    printEndOfMethod();
   }
 
   @Test
   public void testDecodeJMPAndAddress() {
-    System.err.println("not yet implemented");
+    printMethodName();
+    // reset the content of the string builder
+    this.stringBuilder = new StringBuilder("[...]");
+    final BitSet bits = new BitSet();
+
+    bits.set(0, false); //JMPC
+    bits.set(1, false); //JMPN
+    bits.set(2, false); // JMPZ
+    this.instruction = new Mic1Instruction(47, bits, 0);
+    String start = this.stringBuilder.toString();
+    // call decoding
+    this.instruction.decodeJMPAndAddress(this.stringBuilder);
+    // check the created text
+    assertThat(this.stringBuilder.toString()).isEqualTo(start + ";goto 0x2F");
+
+    bits.set(0, true); //JMPC
+    bits.set(1, false); //JMPN
+    bits.set(2, false); // JMPZ
+    this.instruction = new Mic1Instruction(47, bits, 0);
+    start = this.stringBuilder.toString();
+    // call decoding
+    this.instruction.decodeJMPAndAddress(this.stringBuilder);
+    // check the created text
+    assertThat(this.stringBuilder.toString()).isEqualTo(start + ";goto (MBR OR 0x2F)");
+
+    bits.set(0, true); //JMPC
+    bits.set(1, false); //JMPN
+    bits.set(2, false); // JMPZ
+    this.instruction = new Mic1Instruction(0, bits, 0);
+    start = this.stringBuilder.toString();
+    // call decoding
+    this.instruction.decodeJMPAndAddress(this.stringBuilder);
+    // check the created text
+    assertThat(this.stringBuilder.toString()).isEqualTo(start + ";goto (MBR)");
+
+    bits.set(0, false); //JMPC
+    bits.set(1, true); //JMPN
+    bits.set(2, false); // JMPZ
+    this.instruction = new Mic1Instruction(47, bits, 0);
+    start = this.stringBuilder.toString();
+    // call decoding
+    this.instruction.decodeJMPAndAddress(this.stringBuilder);
+    // check the created text
+    assertThat(this.stringBuilder.toString()).isEqualTo("N=" + start + ";if (N) goto 0x12F; else goto 0x2F");
+
+    bits.set(0, false); //JMPC
+    bits.set(1, false); //JMPN
+    bits.set(2, true); // JMPZ
+    this.instruction = new Mic1Instruction(47, bits, 0);
+    start = this.stringBuilder.toString();
+    // call decoding
+    this.instruction.decodeJMPAndAddress(this.stringBuilder);
+    // check the created text
+    assertThat(this.stringBuilder.toString()).isEqualTo("Z=" + start + ";if (Z) goto 0x12F; else goto 0x2F");
+
+    bits.set(0, false); //JMPC
+    bits.set(1, true); //JMPN
+    bits.set(2, false); // JMPZ
+    this.instruction = new Mic1Instruction(447, bits, 0);
+    start = this.stringBuilder.toString();
+    // call decoding
+    this.instruction.decodeJMPAndAddress(this.stringBuilder);
+    // check the created text
+    assertThat(this.stringBuilder.toString()).isEqualTo("N=" + start + ";if (N) goto 0x1BF; else goto 0x1BF");
+
+    bits.set(0, false); //JMPC
+    bits.set(1, false); //JMPN
+    bits.set(2, true); // JMPZ
+    this.instruction = new Mic1Instruction(447, bits, 0);
+    start = this.stringBuilder.toString();
+    // call decoding
+    this.instruction.decodeJMPAndAddress(this.stringBuilder);
+    // check the created text
+    assertThat(this.stringBuilder.toString()).isEqualTo("Z=" + start + ";if (Z) goto 0x1BF; else goto 0x1BF");
+
+    printEndOfMethod();
   }
 
   @Test
   public void testConvertIntToHex() {
-    System.err.println("not yet implemented");
+    printMethodName();
+
+    int i = -42;
+    assertThat(Mic1Instruction.convertIntToHex(i)).isEqualTo("FFFFFFD6");
+
+    i = 42;
+    assertThat(Mic1Instruction.convertIntToHex(i)).isEqualTo("2A");
+
+    i = 4711;
+    assertThat(Mic1Instruction.convertIntToHex(i)).isEqualTo("1267");
+
+    i = -4711;
+    assertThat(Mic1Instruction.convertIntToHex(i)).isEqualTo("FFFFED99");
+
+    i = 0;
+    assertThat(Mic1Instruction.convertIntToHex(i)).isEqualTo("0");
+
+    i = -1;
+    assertThat(Mic1Instruction.convertIntToHex(i)).isEqualTo("FFFFFFFF");
+
+    i = 1;
+    assertThat(Mic1Instruction.convertIntToHex(i)).isEqualTo("1");
+
+    i = Integer.MAX_VALUE;
+    assertThat(Mic1Instruction.convertIntToHex(i)).isEqualTo("7FFFFFFF");
+
+    i = Integer.MIN_VALUE;
+    assertThat(Mic1Instruction.convertIntToHex(i)).isEqualTo("80000000");
+
+    printEndOfMethod();
   }
 
   @Test
   public void testDecodeMemoryBits() {
+    printMethodName();
     final BitSet bits = new BitSet();
 
     // try all combinations of write, read and fetch
@@ -74,13 +192,18 @@ public class Mic1InstructionTest {
           assertThat(this.stringBuilder.toString().contains(";wr")).isEqualTo(write);
           assertThat(this.stringBuilder.toString().contains(";rd")).isEqualTo(read);
           assertThat(this.stringBuilder.toString().contains(";fetch")).isEqualTo(fetch);
+          printStep();
         }
+        printLoopEnd();
       }
+      printLoopEnd();
     }
+    printEndOfMethod();
   }
 
   @Test
   public void testDecodeShifterOperation() {
+    printMethodName();
     final BitSet bits = new BitSet();
 
     // try all combinations of sra1 and sll8
@@ -102,12 +225,16 @@ public class Mic1InstructionTest {
         assertThat(this.stringBuilder.toString()).startsWith(start);
         assertThat(this.stringBuilder.toString().contains(">>1")).isEqualTo(sra1);
         assertThat(this.stringBuilder.toString().contains("<<8")).isEqualTo(sll8);
+        printStep();
       }
+      printLoopEnd();
     }
+    printEndOfMethod();
   }
 
   @Test
   public void testDecodeALUOperation() {
+    printMethodName();
     // reset the content of the string builder
     this.stringBuilder = new StringBuilder("[...]");
 
@@ -156,10 +283,13 @@ public class Mic1InstructionTest {
     this.instruction.decodeALUOperation(this.stringBuilder, "A", "B");
     // check the created text
     assertThat(this.stringBuilder.toString()).isEqualTo(start + "A AND B");
+
+    printEndOfMethod();
   }
 
   @Test
   public void testDecodeALUPlus() {
+    printMethodName();
     final BitSet bits = new BitSet();
 
     for (final boolean enableA : BOOLEAN_POSSIBILITIES) {
@@ -215,14 +345,20 @@ public class Mic1InstructionTest {
             } else if (!enableA && !enableB && !invertA && !increment) {
               assertThat(this.stringBuilder.toString()).isEqualTo(start + "0");
             }
+            printStep();
           }
+          printLoopEnd();
         }
+        printLoopEnd();
       }
+      printLoopEnd();
     }
+    printEndOfMethod();
   }
 
   @Test
   public void testDecodeALUNotB() {
+    printMethodName();
     final BitSet bits = new BitSet();
 
     // test ENB and !ENB
@@ -244,11 +380,14 @@ public class Mic1InstructionTest {
       } else {
         assertThat(this.stringBuilder.toString()).isEqualTo(start + "0");
       }
+      printStep();
     }
+    printEndOfMethod();
   }
 
   @Test
   public void testDecodeALUOr() {
+    printMethodName();
     final BitSet bits = new BitSet();
 
     for (final boolean enableA : BOOLEAN_POSSIBILITIES) {
@@ -286,13 +425,18 @@ public class Mic1InstructionTest {
           } else if (!enableA && !enableB && !invertA) {
             assertThat(this.stringBuilder.toString()).isEqualTo(start + "0");
           }
+          printStep();
         }
+        printLoopEnd();
       }
+      printLoopEnd();
     }
+    printEndOfMethod();
   }
 
   @Test
   public void testDecodeALUAnd() {
+    printMethodName();
     final BitSet bits = new BitSet();
 
     for (final boolean enableA : BOOLEAN_POSSIBILITIES) {
@@ -330,13 +474,18 @@ public class Mic1InstructionTest {
           } else if (!enableA && !enableB && !invertA) {
             assertThat(this.stringBuilder.toString()).isEqualTo(start + "0");
           }
+          printStep();
         }
+        printLoopEnd();
       }
+      printLoopEnd();
     }
+    printEndOfMethod();
   }
 
   @Test
   public void testDecodeBBusBits() {
+    printMethodName();
     checkDecodingOfBBusBits("MDR", 0);
     checkDecodingOfBBusBits("PC", 1);
     checkDecodingOfBBusBits("MBR", 2);
@@ -349,7 +498,9 @@ public class Mic1InstructionTest {
 
     for (int i = 9; i < 16; ++i) {
       checkDecodingOfBBusBits("???", i);
+      printStep();
     }
+    printEndOfMethod();
   }
 
   private void checkDecodingOfBBusBits(final String s, final int i) {
@@ -364,6 +515,7 @@ public class Mic1InstructionTest {
 
   @Test
   public void testDecodeCBusBits() {
+    printMethodName();
     final BitSet bits = new BitSet();
 
     for (final boolean h : BOOLEAN_POSSIBILITIES) {
@@ -409,20 +561,82 @@ public class Mic1InstructionTest {
                                          || this.stringBuilder.toString().contains("=PC=")).isEqualTo(pc);
                       assertThat(this.stringBuilder.toString().contains("SP=")).isEqualTo(sp);
                       assertThat(this.stringBuilder.toString().contains("TOS=")).isEqualTo(tos);
+                      printStep();
                     }
+                    printLoopEnd();
                   }
+                  printLoopEnd();
                 }
+                printLoopEnd();
               }
+              printLoopEnd();
             }
+            printLoopEnd();
           }
+          printLoopEnd();
         }
+        printLoopEnd();
       }
+      printLoopEnd();
     }
+    printEndOfMethod();
   }
 
   @Test
-  public void testEqualsObject() {
-    System.err.println("not yet implemented");
+  public void testHashCodeAndEqualsObject() {
+    printMethodName();
+
+    assertThat(this.instruction).isNotEqualTo(null);
+    assertThat(this.instruction).isNotEqualTo("...");
+    assertThat(this.instruction).isEqualTo(this.instruction);
+    assertThat(this.instruction.hashCode()).isEqualTo(this.instruction.hashCode());
+
+    int addr = 0;
+    int b = 0;
+    final BitSet bs = new BitSet();
+
+    Mic1Instruction other = new Mic1Instruction(addr, bs, b);
+    assertThat(this.instruction).isEqualTo(other);
+    assertThat(this.instruction.hashCode()).isEqualTo(this.instruction.hashCode());
+
+    b = 1;
+    this.instruction = new Mic1Instruction(addr, bs, b);
+    assertThat(this.instruction).isNotEqualTo(other);
+    assertThat(this.instruction.hashCode()).isNotEqualTo(other.hashCode());
+    other = new Mic1Instruction(addr, bs, b); // make object equal to instruction
+
+    addr = 17;
+    this.instruction = new Mic1Instruction(addr, bs, b);
+    assertThat(this.instruction).isNotEqualTo(other);
+    assertThat(this.instruction.hashCode()).isNotEqualTo(other.hashCode());
+    other = new Mic1Instruction(addr, bs, b); // make object equal to instruction
+
+    for (int i = 0; i < 23; ++i) {
+      bs.set(i);
+      this.instruction = new Mic1Instruction(addr, bs, 1);
+      assertThat(this.instruction).isNotEqualTo(other);
+      assertThat(this.instruction.hashCode()).isNotEqualTo(other.hashCode());
+      other = new Mic1Instruction(addr, bs, 1); // make object equal to instruction
+      printStep();
+    }
+
+    printEndOfMethod();
   }
 
+  private void printEndOfMethod() {
+    System.out.println();
+  }
+
+  private void printLoopEnd() {
+    System.out.print(" ");
+  }
+
+  private void printStep() {
+    System.out.print(".");
+  }
+
+  private void printMethodName() {
+    System.out.print(this.getClass().getSimpleName() + "#");
+    System.out.print(Thread.currentThread().getStackTrace()[2].getMethodName() + " ");
+  }
 }
