2b898a0a2bca7b9cfd82afd2f75ae2c8674839b8
croesch <christianroesch@gmx.net>
2011-11-21 19:59:36 +0100

    Implemented first sketch of the processor.

---
+++ src/main/java/com/github/croesch/mic1/Mic1.java
@@ -0,0 +1,158 @@
+package com.github.croesch.mic1;
+
+import java.io.InputStream;
+
+import com.github.croesch.error.FileFormatException;
+import com.github.croesch.mic1.alu.Alu;
+import com.github.croesch.mic1.controlstore.Mic1ALUSignalSet;
+import com.github.croesch.mic1.controlstore.Mic1CBusSignalSet;
+import com.github.croesch.mic1.controlstore.Mic1ControlStore;
+import com.github.croesch.mic1.controlstore.Mic1Instruction;
+import com.github.croesch.mic1.controlstore.Mic1JMPSignalSet;
+import com.github.croesch.mic1.mpc.NextMPCCalculator;
+import com.github.croesch.mic1.register.Register;
+import com.github.croesch.mic1.shifter.Shifter;
+
+/**
+ * This class represents the CISC-processor being debugged by this program.
+ * 
+ * @author croesch
+ * @since Date: Nov 20, 2011
+ */
+public final class Mic1 {
+
+  /** the ALU of the processor */
+  private final Alu alu = new Alu();
+
+  /** the shifter belonging to the ALU of the processor */
+  private final Shifter shifter = new Shifter();
+
+  /** calculator for next MPC value */
+  private final NextMPCCalculator mpcCalculator = new NextMPCCalculator();
+
+  /** store of the micro program */
+  private final Mic1ControlStore controlStore;
+
+  /** current instruction */
+  private Mic1Instruction instruction = null;
+
+  /**
+   * Constructs a new Mic1-processor, reading the given inputstreams as micro-program and assembler-program.
+   * 
+   * @since Date: Nov 21, 2011
+   * @param micAsm the micro-assembler-program
+   * @param asm the assembler-program
+   * @throws FileFormatException if the format of either the assembler- or the micro-assembler-program is incorrect.
+   */
+  public Mic1(final InputStream micAsm, final InputStream asm) throws FileFormatException {
+    this.controlStore = new Mic1ControlStore(micAsm);
+  }
+
+  /**
+   * Performs one execution of the processor.
+   * 
+   * @since Date: Nov 21, 2011
+   */
+  public void doTick() {
+    doClock1();
+    doClock2();
+    doClock3();
+  }
+
+  /**
+   * Performs the execution of the first part of a instruction - clock number one.
+   * 
+   * @since Date: Nov 21, 2011
+   */
+  private void doClock1() {
+    // fetching instruction
+    this.instruction = this.controlStore.getInstruction(this.mpcCalculator.getMpc());
+    final Mic1ALUSignalSet aluSignals = this.instruction.getAluSignals();
+    // setting the signals
+    this.alu.setEnA(aluSignals.isEnA()).setEnB(aluSignals.isEnB());
+    this.alu.setF0(aluSignals.isF0()).setF1(aluSignals.isF1());
+    this.alu.setInc(aluSignals.isInc()).setInvA(aluSignals.isInvA());
+    // set A and B-Bus
+    this.alu.setA(Register.H.getValue());
+    this.alu.setB(this.instruction.getbBusSelect().getValue());
+    // run ALU
+    this.alu.calculate();
+
+    // setting signals
+    this.shifter.setSLL8(aluSignals.isSLL8());
+    this.shifter.setSRA1(aluSignals.isSRA1());
+    // setting input from ALU
+    this.shifter.setInput(this.alu.getOut());
+    // run shifter
+    this.shifter.calculate();
+  }
+
+  /**
+   * Performs the execution of the first part of a instruction - clock number two.
+   * 
+   * @since Date: Nov 21, 2011
+   */
+  private void doClock2() {
+    setValueIntoRegisters(this.shifter.getOutput(), this.instruction.getCBusSignals());
+
+    // set N and Z
+    this.mpcCalculator.setN(this.alu.isN());
+    this.mpcCalculator.setZ(this.alu.isZ());
+    // TODO fill MBR and MDR
+  }
+
+  /**
+   * Performs the execution of the first part of a instruction - clock number three.
+   * 
+   * @since Date: Nov 21, 2011
+   */
+  private void doClock3() {
+    this.mpcCalculator.setAddr(this.instruction.getNextAddress());
+    this.mpcCalculator.setMbr((byte) Register.MBR.getValue());
+
+    final Mic1JMPSignalSet jmpSignals = this.instruction.getJmpSignals();
+    this.mpcCalculator.setJmpC(jmpSignals.isJmpC());
+    this.mpcCalculator.setJmpN(jmpSignals.isJmpN());
+    this.mpcCalculator.setJmpZ(jmpSignals.isJmpZ());
+    this.mpcCalculator.calculate();
+
+    // TODO set signals read, write and fetch and read value
+  }
+
+  /**
+   * Sets the value of the C-Bus into the selected registers.
+   * 
+   * @since Date: Nov 21, 2011
+   * @param value the value of the C-Bus.
+   * @param cBusSignals the signals that determine, which registers are selected.
+   */
+  private static void setValueIntoRegisters(final int value, final Mic1CBusSignalSet cBusSignals) {
+    if (cBusSignals.isCpp()) {
+      Register.CPP.setValue(value);
+    }
+    if (cBusSignals.isH()) {
+      Register.H.setValue(value);
+    }
+    if (cBusSignals.isLv()) {
+      Register.LV.setValue(value);
+    }
+    if (cBusSignals.isMar()) {
+      Register.MAR.setValue(value);
+    }
+    if (cBusSignals.isMdr()) {
+      Register.MDR.setValue(value);
+    }
+    if (cBusSignals.isOpc()) {
+      Register.OPC.setValue(value);
+    }
+    if (cBusSignals.isPc()) {
+      Register.PC.setValue(value);
+    }
+    if (cBusSignals.isSp()) {
+      Register.SP.setValue(value);
+    }
+    if (cBusSignals.isTos()) {
+      Register.TOS.setValue(value);
+    }
+  }
+}
