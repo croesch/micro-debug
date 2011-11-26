9d55f7536797370b2ca8b9a889931644b293afe8
croesch <christianroesch@gmx.net>
2011-11-26 15:18:53 +0100

    Added GPL note to each file of project.

--- src/test/java/com/github/croesch/TestUtil.java
+++ src/test/java/com/github/croesch/TestUtil.java
@@ -1,9 +1,27 @@
+/*
+ * Copyright (C) 2011-2012  Christian Roesch
+ * 
+ * This file is part of micro-debug.
+ * 
+ * micro-debug is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * micro-debug is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with micro-debug.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.github.croesch;
 
 import org.junit.Ignore;
 
 /**
- * TODO Comment here ...
+ * This class contains utility methods for the tests in this project. It doesn't contain any test methods.
  * 
  * @author croesch
  * @since Date: Nov 12, 2011
