88b7edfc250d74b6b6e31c4700fab1b549760107
croesch <christianroesch@gmx.net>
2012-01-27 22:56:18 +0100

    Added manager for break points

---
+++ src/main/java/com/github/croesch/mic1/BreakPointManager.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2011-2012  Christian Roesch
+ * 
+ * This file is part of micro-debug.
+ * 
+ * micro-debug is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * micro-debug is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with micro-debug.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.croesch.mic1;
+
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.List;
+
+import com.github.croesch.mic1.register.Register;
+
+/**
+ * A manager for break points in the debugger.
+ * 
+ * @author croesch
+ * @since Date: Jan 27, 2012
+ */
+final class BreakPointManager {
+
+  /** contains a list of values for each register that are set to be a breakpoint */
+  private final EnumMap<Register, List<Integer>> registerBreakPoints = new EnumMap<Register, List<Integer>>(Register.class);
+
+  /**
+   * Constructs the break point manager.
+   * 
+   * @since Date: Jan 27, 2012
+   */
+  public BreakPointManager() {
+    for (final Register r : Register.values()) {
+      this.registerBreakPoints.put(r, new ArrayList<Integer>());
+    }
+  }
+
+  /**
+   * Returns whether any break point condition is met.
+   * 
+   * @since Date: Jan 27, 2012
+   * @return <code>true</code> if a break point is met,<br>
+   *         <code>false</code> otherwise
+   */
+  boolean isBreakPoint() {
+    for (final Register r : Register.values()) {
+      if (this.registerBreakPoints.get(r).contains(Integer.valueOf(r.getValue()))) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Adds a breakpoint for the given {@link Register} and the given value.
+   * 
+   * @since Date: Jan 27, 2012
+   * @param r the {@link Register} to watch for the given value
+   * @param val the value the debugger should break if the given {@link Register} has it.
+   */
+  void addBreakPoint(final Register r, final int val) {
+    if (r != null) {
+      this.registerBreakPoints.get(r).add(Integer.valueOf(val));
+    }
+  }
+}
---
+++ src/test/java/com/github/croesch/mic1/BreakPointManagerTest.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2011-2012  Christian Roesch
+ * 
+ * This file is part of micro-debug.
+ * 
+ * micro-debug is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * micro-debug is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with micro-debug.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.croesch.mic1;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import org.junit.Test;
+
+import com.github.croesch.DefaultTestCase;
+import com.github.croesch.TestUtil;
+import com.github.croesch.mic1.register.Register;
+
+/**
+ * Contains test cases for {@link BreakPointManager}.
+ * 
+ * @author croesch
+ * @since Date: Jan 27, 2012
+ */
+public class BreakPointManagerTest extends DefaultTestCase {
+
+  private BreakPointManager bpm;
+
+  @Override
+  protected void setUpDetails() {
+    this.bpm = new BreakPointManager();
+  }
+
+  @Test
+  public void testAddBreakPoint() {
+    // shouldn't throw any exception
+    this.bpm.addBreakPoint(null, 0);
+  }
+
+  @Test
+  public void testIsBreakPoint() {
+    TestUtil.printMethodName();
+
+    for (final Register r : Register.values()) {
+      r.setValue(0);
+    }
+    assertThat(this.bpm.isBreakPoint()).isFalse();
+    for (final Register r : Register.values()) {
+      this.bpm.addBreakPoint(r, 1);
+      assertThat(this.bpm.isBreakPoint()).isFalse();
+      r.setValue(1);
+      assertThat(this.bpm.isBreakPoint()).isTrue();
+      r.setValue(0);
+      TestUtil.printStep();
+    }
+    TestUtil.printLoopEnd();
+    for (final Register r : Register.values()) {
+      this.bpm.addBreakPoint(r, 2);
+      assertThat(this.bpm.isBreakPoint()).isFalse();
+      r.setValue(1);
+      assertThat(this.bpm.isBreakPoint()).isTrue();
+      r.setValue(2);
+      assertThat(this.bpm.isBreakPoint()).isTrue();
+      r.setValue(0);
+      TestUtil.printStep();
+    }
+    TestUtil.printEndOfMethod();
+  }
+}
